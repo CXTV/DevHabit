280a8862474e2b2f073bc474c6c54e3adff17dd5
fxx <394967886@qq.com>
2025-04-12 21:19:49 +1200

    added getCurrentUser

--- DevHabit/DevHabit.Api/Controllers/UsersController.cs
+++ DevHabit/DevHabit.Api/Controllers/UsersController.cs
@@ -1,14 +1,19 @@
-﻿using DevHabit.Api.Database;
+﻿using System.Security.Claims;
+using DevHabit.Api.Database;
 using DevHabit.Api.DTOs.Users;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DevHabit.Api.Controllers;
 
 
+[Authorize]
 [ApiController]
 [Route("users")]
-public sealed class UsersController(ApplicationDbContext dbContext) : ControllerBase
+public sealed class UsersController(
+    ApplicationDbContext dbContext
+    ) : ControllerBase
 {
     [HttpGet("{id}")]
     public async Task<ActionResult<UserDto>> GetUserById(string id)
@@ -26,4 +31,27 @@ public sealed class UsersController(ApplicationDbContext dbContext) : Controller
         return Ok(user);
     }
 
+
+    [HttpGet("me")]
+    public async Task<ActionResult<UserDto>> GetCurrentUser()
+    {
+        //string? userId = await userContext.GetUserIdAsync();
+        string? identityId = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (string.IsNullOrWhiteSpace(identityId))
+        {
+            return Unauthorized();
+        }
+
+        UserDto? user = await dbContext.Users
+            .Where(u => u.IdentityId == identityId)
+            .Select(UserQueries.ProjectToDto())
+            .FirstOrDefaultAsync();
+
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(user);
+    }
 }
